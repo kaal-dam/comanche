2d519781b704a4b95c027dcf00473a25b372acbf
Damien TOULOUSE <toulousd@hevea03.iut-infobio.priv.univ-lille1.fr>
2015-01-19 16:34:46 +0100

    creation du serveur dans le main main.c

--- webserver/main.c
+++ webserver/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include "socket.h"
 int main(int argc, char ** argv){
-    /*Arnold Robbins in the LJ of February '95 describing RCS*/
-    if(argc>1 && strcmp(argv[1], "-advice")==0){
-        printf("Don't Panic!\n");
-        return 42;
+    int serveur = cree_serveur(8000);
+    if(serveur == -1){
+        printf("erreur de creation serveur");
+        fflush(stdout);
+        return -1;
     }
-    printf("Need an advice?\n");
+    /*
+     *faire la partie socket client
+     */
     return 0;
 }
