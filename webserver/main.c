5b2b2b94addcb90c29056c17ba53f0a0e6569eb5
Maverick59 <alexandre.cuvillier59@gmail.com>
2015-01-21 20:49:45 +0100

    finition main.c

--- webserver/main.c
+++ webserver/main.c
@@ -2,20 +2,19 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <errno.h>
+#include <unistd.h>
 #include "socket.h"
 
-int main(int argc, char ** argv){
-    int serveur = cree_serveur(8000);
+int main(){
+    int serveur = creer_serveur(8000);
     if(serveur == -1){
         printf("erreur de creation serveur");
         fflush(stdout);
         return -1;
     }
-    /*
-     *finir la partie socket client
-     */
     /*accepter les connexions indefiniment*/
-    while(1==1){
+    while(1){
         int fd;
         fd=accept(serveur, NULL, NULL);
         if(fd==-1){
@@ -23,10 +22,25 @@ int main(int argc, char ** argv){
             return -1;
         }else{
             /*Connexion d'un client, message de bienvenue*/
-            printf("Bienvenue sur mon serveur aventurier! Serais tu un ennemi des comanches? si tel est le cas, je te conseille de te deconnecter très vite de ce serveur.. sinon, bienvenue a toi dans notre tribu, tu seras notre invite jusqu'a ce que tu decides de couper la connection qui nous lie. Nous t'offrirons tout ce dont tu as besoin, il te suffira de demander. Prends garde, les comanches sont de vieux indiens rusés et ils adorent faire des farces! ne t'etonne pas si certains de tes fichiers disparaissent ou bien si certains fichiers se creent tout seul sur ton ordinateur, ce ne sera jamais rien de mechant tu peux me faire confiance. Si tu as un probleme quelconque, viens donc voir kaal-dam, le chef de notre tribu, il pourra te depanner avec ses super pouvoirs! sinon, contactes maverick, ce vieux bougre n'est jamais loin et en general il est toujours volontaire pour aider! Evites aussi de parler aux comanches qui sont trop vieux comme serveuris qui t'heberge actuellement, il a tendance a repeter betement tout ce qu'on lui dit, et le pire c'est que ça l'amuse..");
+            char *s;
+            int nb_lus;
+            int stop=0;
+            s="Bienvenue sur mon serveur aventurier! Serais tu un ennemi des comanches? si tel est le cas, je te conseille de te deconnecter très vite de ce serveur.. sinon, bienvenue a toi dans notre tribu, tu seras notre invite jusqu'a ce que tu decides de couper la connection qui nous lie. Nous t'offrirons tout ce dont tu as besoin, il te suffira de demander. Prends garde, les comanches sont de vieux indiens rusés et ils adorent faire des farces! ne t'etonne pas si certains de tes fichiers disparaissent ou bien si certains fichiers se creent tout seul sur ton ordinateur, ce ne sera jamais rien de mechant tu peux me faire confiance. Si tu as un probleme quelconque, viens donc voir kaal-dam, le chef de notre tribu, il pourra te depanner avec ses super pouvoirs! sinon, contactes maverick, ce vieux bougre n'est jamais loin et en general il est toujours volontaire pour aider! Evites aussi de parler aux comanches qui sont trop vieux comme serveuris qui t'heberge actuellement, il a tendance a repeter betement tout ce qu'on lui dit, et le pire c'est que ça l'amuse..";
+            if(write(fd, s, strlen(s))==-1){
+                perror("message d'accueil");
+                return -1;
+            }
             /*tant que le client est connecte, on repete ce qu'il nous envoie*/
-            while(ECONNABORTED){
-                
+            while(stop==0){
+                char * s2;
+                nb_lus=read(fd, &s2, 512);
+                if(nb_lus<1){
+                    perror("lecture");
+                    stop=1;
+                }
+                if(write(fd, &s2, nb_lus)==-1){
+                    perror("ecriture");
+                 }
             }
         }
     }
