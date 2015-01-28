a282fa58778196d3ad5a33892df5fdbda33ce5a6
Maverick59 <alexandre.cuvillier59@gmail.com>
2015-01-28 18:04:19 +0100

    modifications pour inclure fprintf, fgets. ne fonctionne pas pour le moment

--- webserver/main.c
+++ webserver/main.c
@@ -57,26 +57,33 @@ int main(){
             }
             /*Dans le process fils*/
             if(pid==0){
-                /*Connexion d'un client, message de bienvenue*/
-                char *s, *s2;
-                int nb_lus;
-                s="Bienvenue sur mon serveur aventurier! Serais tu un ennemi des comanches? si tel est le cas, je te conseille de te deconnecter très vite de ce serveur.. sinon, bienvenue a toi dans notre tribu, tu seras notre invite jusqu'a ce que tu decides de couper la connection qui nous lie. Nous t'offrirons tout ce dont tu as besoin, il te suffira de demander. Prends garde, les comanches sont de vieux indiens rusés et ils adorent faire des farces! ne t'etonne pas si certains de tes fichiers disparaissent ou bien si certains fichiers se creent tout seul sur ton ordinateur, ce ne sera jamais rien de mechant tu peux me faire confiance. Si tu as un probleme quelconque, viens donc voir kaal-dam, le chef de notre tribu, il pourra te depanner avec ses super pouvoirs! sinon, contactes maverick, ce vieux bougre n'est jamais loin et en general il est toujours volontaire pour aider! Evites aussi de parler aux comanches qui sont trop vieux comme serveuris qui t'heberge actuellement, il a tendance a repeter betement tout ce qu'on lui dit, et le pire c'est que ça l'amuse..";
-                //while(1){
-                if(write(fd, s, strlen(s))==-1){
-                    perror("message d'accueil");
-                    return -1;
+                const char * mode;
+                /*en mode lecture et ecriture*/
+                mode="w+";
+                char *s;
+                FILE * s2=NULL;
+                /*obtenir une structure FILE * a partir du descripteur correspondant a la socket client*/
+                s2=fdopen(fd, mode);
+                if(s2==NULL){
+                    perror("fdopen");
+                    exit(-1);
                 }
+                /*Message de bienvenue*/
+                s="Bienvenue sur mon serveur aventurier! Serais tu un ennemi des comanches? si tel est le cas, je te conseille de te deconnecter très vite de ce serveur.. sinon, bienvenue a toi dans notre tribu, tu seras notre invite jusqu'a ce que tu decides de couper la connection qui nous lie. Nous t'offrirons tout ce dont tu as besoin, il te suffira de demander. Prends garde, les comanches sont de vieux indiens rusés et ils adorent faire des farces! ne t'etonne pas si certains de tes fichiers disparaissent ou bien si certains fichiers se creent tout seul sur ton ordinateur, ce ne sera jamais rien de mechant tu peux me faire confiance. Si tu as un probleme quelconque, viens donc voir kaal-dam, le chef de notre tribu, il pourra te depanner avec ses super pouvoirs! sinon, contactes maverick, ce vieux bougre n'est jamais loin et en general il est toujours volontaire pour aider! Evites aussi de parler aux comanches qui sont trop vieux comme serveuris qui t'heberge actuellement, il a tendance a repeter betement tout ce qu'on lui dit, et le pire c'est que ça l'amuse..\n";
+                /*Boucle while pour tester l'erreur SIGPIPE*/
+                //while(1){
+                if(fprintf(s2, s)<0){
+                    exit(-1);
+                }
+                s="";
                 //}
                 /*tant que le client est connecte, on repete ce qu'il nous envoie*/
-                while((nb_lus=read(fd, &s2, 512))>0){
-                    if(write(fd, &s2, nb_lus)==-1){
-                        perror("ecriture");
-                        return -1;
+                /*Marche pas encore avec fgets*/
+                while((s=fgets(s, 512, s2))!=NULL){
+                    fprintf(s2, "coucou 1!");
+                    if(fprintf(s2, s)<0){
+                        exit(-1);
                     }
-                }
-                if(nb_lus<0){
-                    perror("lecture");
-                    return -1;
                 }
                 /*fils termine*/
                 exit(0);
