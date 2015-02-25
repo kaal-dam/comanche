f8f7da19f132601fc201ff9a0a767d446456186b
Maverick59 <alexandre.cuvillier59@gmail.com>
2015-02-25 09:09:05 +0100

    debut root_directory

---
+++ webserver/my_file.c
@@ -0,0 +1,8 @@
+#include "file.h"
+
+int check_and_open(const char *url, const char *document_root){
+    if(fdopen()){
+
+    }
+    return 0;
+}
---
+++ webserver/my_file.h
@@ -0,0 +1,1 @@
+int check_and_open(const char *url, const char *document_root);
