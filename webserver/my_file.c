318ed09ece4436fc6be2a38635d8d4d3db3f19e6
kaal-dam <dam.toulouse@gmail.com>
2015-02-25 10:19:38 +0100

    légère correction check_and_update

--- webserver/my_file.c
+++ webserver/my_file.c
@@ -4,15 +4,31 @@
 int check_and_open(const char *url, const char *document_root){
     int fdFile;
     char tmp[512];
+    struct stat file_stat;
     strcpy(tmp,document_root);
     strcat(tmp,url);
     
     if(open(document_root,O_RDONLY)!=-1){
-        if((fdFile=open(tmp,O_RDONLY))!=-1){
-            return fdFile;
+       /*recuperation des infos sur le dossier*/
+        if(stat(tmp, &file_stat)!=-1){
+            /*Verif document_root est un dossier*/
+            if(S_ISREG(file_stat.st_mode)){
+                if((fdFile=open(tmp,O_RDONLY))!=-1){
+                    
+                    return fdFile;
+                }
+                else{
+                    perror("open file");
+                    return -1;
+                }
+            }
+            else{
+                printf("%s n'est pas un fichier valide!\n", url);
+                return -1;
+            }
         }
         else{
-            perror("open file");
+            perror("stat");
             return -1;
         }
     }
