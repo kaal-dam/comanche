770db9f1f28ce01792d8d9ec52c622b28dd0ce24
Maverick59 <alexandre.cuvillier59@gmail.com>
2015-02-25 09:31:16 +0100

    renommage file.c et file.h

--- webserver/main.c
+++ webserver/main.c
@@ -11,40 +11,48 @@ int main(int argc, char *argv[]){
     struct stat file_stat;
     /*repertoire racine du serveur*/
     const char *document_root="../html";
+    /*recuperation des infos sur le dossier*/
     if(stat(document_root, &file_stat)!=-1){
+        /*Verif document_root est un dossier*/
         if(S_ISDIR(file_stat.st_mode)){
-            initialiser_signaux();
-		    int serveur = creer_serveur(8080);
-		    if(serveur == -1){
-		        printf("erreur de creation serveur\n");
-		        fflush(stdout);
-		        return -1;
-		    }
-		    /*accepter les connexions indefiniment*/
-		    while(1){
-		        /*descripteur de fichier correspondant a la socket cliente*/
-		        int client;
-		        /*Attente d'une connexion (bloquant)*/
-		        client=accept(serveur, NULL, NULL);
-		        if(client==-1){
-		            perror("connection to server fail");
+            /*verif droit lecture sur le dossier*/
+            if(access(document_root, R_OK)==0){
+                initialiser_signaux();
+		        int serveur = creer_serveur(8080);
+		        if(serveur == -1){
+		            printf("erreur de creation serveur\n");
+		            fflush(stdout);
 		            return -1;
-		        }else{
-		            int pid=fork();
-		            /*fork foire*/
-		            if(pid==-1){
-		                perror("fork");
+		        }
+		        /*accepter les connexions indefiniment*/
+		        while(1){
+		            /*descripteur de fichier correspondant a la socket cliente*/
+		            int client;
+		            /*Attente d'une connexion (bloquant)*/
+		            client=accept(serveur, NULL, NULL);
+		            if(client==-1){
+		                perror("connection to server fail");
 		                return -1;
-		            }
-		            /*Dans le process fils*/
-		            if(pid==0){
-		                traitement_fils(client, document_root);
 		            }else{
-		                /*Dans le pere*/
-		                close(client);
+		                int pid=fork();
+		                /*fork foire*/
+		                if(pid==-1){
+		                    perror("fork");
+		                    return -1;
+		                }
+		                /*Dans le process fils*/
+		                if(pid==0){
+		                    traitement_fils(client, document_root);
+		                }else{
+		                    /*Dans le pere*/
+		                    close(client);
+		                }
 		            }
 		        }
-		    }
+            }else{
+                perror("access");
+                return -1;
+            }
         }else{
             printf("%s n'est pas un repertoire valide! arret du serveur\n", document_root);
             return -1;
--- webserver/my_file.c
+++ webserver/my_file.c
@@ -1,4 +1,4 @@
-#include "file.h"
+#include "my_file.h"
 
 int check_and_open(const char *url, const char *document_root){
     if(fdopen()){
